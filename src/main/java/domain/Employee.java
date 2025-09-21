7ce5421657ab131092de4003cdd2fe3405bba7e9
minjin0610 <138571857+minjin0610@users.noreply.github.com>
2025-09-21 18:15:39 +0900

    feat : 직급 타입 enum 구현

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -1,21 +1,20 @@
 package domain;
 
+import domain.enums.EmployeeType;
+
 public class Employee extends Person{
 
     protected int salary;
     protected Integer teamId;
+    protected EmployeeType employeeType;
 
-    public Employee(int id, String name, int salary) {
+    public Employee(int id, String name, int salary, EmployeeType employeeType) {
         super(id, name);
         this.salary = salary;
+        this.employeeType = employeeType;
     }
 
     public void assignTeam(int teamId) {
         this.teamId = teamId;
     }
-
-    public Integer getTeamId() {
-        return teamId;
-    }
-
 }
